3b6d47c2632ff30e4dc1ef21ea4d797568d45a4f
Pontus Hojer <pontushojer@gmail.com>
2021-04-08 13:09:44 +0200

    Update getfrags.py

--- src/dbsocp/cli/getfrags.py
+++ src/dbsocp/cli/getfrags.py
@@ -4,6 +4,7 @@ Convert BAM to Fragment file (BED)
 For info on Fragment files go to:
 https://support.10xgenomics.com/single-cell-atac/software/pipelines/latest/output/fragments
 """
+from itertools import takewhile
 import logging
 from collections import Counter
 import sys
@@ -11,7 +12,7 @@ import sys
 import pysam
 from xopen import xopen
 
-from dbsocp.utils import tqdm
+from dbsocp.utils import tqdm, Summary
 
 logger = logging.getLogger(__name__)
 
@@ -35,34 +36,81 @@ def add_arguments(parser):
 
 def main(args):
     cache = Counter()
+    summary = Summary()
     current_chrom = None
     with xopen(args.output, "w") as outfile:
         for read, mate in parse_pairs(args.input):
-            if read.mapping_quality < args.min_mapq or \
-                    mate.mapping_quality < args.min_mapq:
+            summary["Pairs read"] += 1
+            if skip_pair(read, mate, args.min_mapq):
+                summary["Pairs skipped"] += 1
                 continue
 
-            # Get Tn5 adjusted positions
-            start = min(mate.reference_start, read.reference_start) + 4
-            end = max(mate.reference_end, read.reference_end) - 5
+            # Get 5-prime coordinates adjusted for soft-clipping
+            read_5prime = compute_five_prime_coords(read)
+            mate_5prime = compute_five_prime_coords(mate)
+
+            # Get Tn5 adjusted start and end
+            start = min(read_5prime, mate_5prime) + 4
+            end = max(read_5prime, mate_5prime) - 5
 
             barcode = read.get_tag(args.barcode_tag)
             chromosome = read.reference_name
 
             if chromosome != current_chrom:
-                write_chromosome(outfile, cache, current_chrom)
+                write_chromosome(outfile, cache, current_chrom, summary)
                 current_chrom = chromosome
                 cache.clear()
 
             cache[(start, end, barcode)] += 1
 
-        write_chromosome(outfile, cache, current_chrom)
+        write_chromosome(outfile, cache, current_chrom, summary)
+
+    summary["Duplication rate"] = 100 * summary["Pairs duplicate"] / \
+                                  (summary["Pairs read"] - summary["Pairs skipped"])
+    summary.print_stats()
 
 
-def write_chromosome(file, cache, chromosome):
+def skip_pair(read, mate, threshold):
+    """
+    Skip read pair if
+    - Either is unmapped
+    - Not proper pair
+    - Either has low mapping quality
+    - Mapped to different chromosomes
+    - Both mapped to same strand
+    """
+    return (read.is_unmapped or mate.is_unmapped) or \
+           (not read.is_proper_pair) or \
+           (read.mapping_quality < threshold or read.mapping_quality < threshold) or \
+           (read.reference_name != mate.reference_name) or \
+           (read.is_reverse == mate.is_reverse)
+
+
+def compute_five_prime_coords(read):
+    """
+    Computes the 5' position in chromosome coordinates
+    Assumes that the read is mapped and the CIGAR exists.
+
+    From CellRanger-ATAC:
+    https://github.com/10XGenomics/cellranger-atac/blob/5753003c286de55c193f02e1d66c847cb635aa60/lib/python/tools/peaks.py#L71
+    """
+    cigar = read.cigartuples
+    if read.is_reverse:
+        # Add the suffix clip to the alignment end position
+        suffix_clip = sum([x[1] for x in takewhile(lambda x: x[0] == 4, reversed(cigar))])
+        return read.reference_end + suffix_clip
+    else:
+        # Subtract the prefix clip from the alignment position
+        prefix_clip = sum([x[1] for x in takewhile(lambda x: x[0] == 4, cigar)])
+        return read.reference_start - prefix_clip
+
+
+def write_chromosome(file, cache, chromosome, summary):
     if chromosome is not None:
         lines = [(chromosome, *pos, count) for pos, count in cache.items()]
         for line in sorted(lines, key=lambda x: (x[1], x[2])):
+            summary["Fragments written"] += 1
+            summary["Pairs duplicate"] += line[-1] - 1
             print(*line, sep="\t", file=file)
 
 
@@ -82,9 +130,6 @@ def parse_pairs(bam_file: str):
             if read.query_name in cache:
                 yield read, cache.pop(read.query_name)
             else:
-                if not read.is_unmapped and \
-                        not read.mate_is_unmapped and \
-                        read.is_proper_pair:
-                    cache[read.query_name] = read
+                cache[read.query_name] = read
     cache.clear()
     pysam.set_verbosity(save)
